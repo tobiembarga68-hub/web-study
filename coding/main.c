0fe5777625bb6779cb9676359f3b56c636df9ad5
tobiembarga68-hub <tobiembarga68@gmail.com>
2026-06-26 04:31:05 +0100

    add files

---
+++ coding/main.c
@@ -0,0 +1,29 @@
+  #include <stdio.h> 
+
+    int main () {
+
+    int age = 25;
+    int year = 2025;
+    float pi = 26.456;
+    float md = 35.73;
+    double td = 3.002828187663;
+    double fd = 4.637839893199;
+
+    printf("You are %d year old\n", age);
+    printf("The year is %d\n", year);
+    printf("You have attend %f of percentage\n", pi);
+    printf("The remaining percentage is %f\n", md);
+    printf("The is %.15lf with you\n", td);
+    printf("You need to give %.6lf\n", fd);
+
+    char garde = 'A';
+    char symbol = '!';
+    char currunry = '$';
+
+    printf("Your grade is %c\n", garde);
+    printf("Your favourable symbol %c\n", symbol);
+    printf("Amount of prices %c16000\n", currunry);
+
+
+    return 0;
+} 
